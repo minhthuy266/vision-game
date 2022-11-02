bc59eec64df4e09f450020c44f0c70cb5ae5a56b
thuynm <thuynm@visionnetwork.vn>
2022-11-02 17:58:29 +0700

    Added sharp package

--- components/NewsScreenComponents/TopBanner/index.js
+++ components/NewsScreenComponents/TopBanner/index.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Banner from "public/assets/images/NewsScreen_Banner1.png";
 import NewsTabs from "../NewsTabs";
+import { StyledBannerWrapper } from "./styles";
 
 export const TopBanner = () => {
   return (
-    <div>
+    <StyledBannerWrapper>
       <Image src={Banner} alt="News Screen Banner" />
 
       <NewsTabs />
-    </div>
+    </StyledBannerWrapper>
   );
 };
---
+++ components/NewsScreenComponents/TopBanner/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledBannerWrapper = styled.div`
+  img {
+    width: 100vw;
+    height: auto;
+    object-fit: cover;
+  }
+`;
